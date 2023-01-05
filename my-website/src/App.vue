7485f16d072b71f37c6f6971313f364708c82645
shimoncohen <shimoncohen655@gmail.com>
2023-01-05 23:59:13 +0200

    add( added first webpage template)

---
+++ my-website/src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-view />
+    </nav>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ my-website/src/components/TheWelcome.vue
@@ -0,0 +1,9 @@
+<script >
+</script>
+
+<template>
+  <div>
+    <h1 class="display-1 text-center">Express your buissness</h1>
+    <h1 class="display-1 text-center">with a custom website.</h1>
+  </div>
+</template>
