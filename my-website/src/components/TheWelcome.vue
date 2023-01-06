f8e6fea7004fd759bed758fbe40b8732a8c08561
shimoncohen <shimoncohen655@gmail.com>
2023-01-07 01:59:42 +0200

    feat(black background to text in TheWelcome commponnent)

--- my-website/src/components/TheWelcome.vue
+++ my-website/src/components/TheWelcome.vue
@@ -1,9 +1,14 @@
-<script >
+<script>
 </script>
 
 <template>
-  <div>
-    <h1 class="display-1 text-center">Express your buissness</h1>
-    <h1 class="display-1 text-center">with a custom website.</h1>
+  <div class="display-1 text-center text-bg-dark p-3">
+    <h1 class="display-1">Express your buissness</h1>
+    <h1 class="display-3 text-center text-bg-secondary p-3">with a custom website.</h1>
+    <hr class="border border-danger border-2 opacity-50">
   </div>
 </template>
+
+<style>
+
+</style>
